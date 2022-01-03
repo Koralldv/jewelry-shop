51e47be4c4e6abccfc2c771fe46d66179b2450c6
Koralldv <52466177+Koralldv@users.noreply.github.com>
2022-01-03 15:23:14 +0300

    Добаваи главную страницу

---
+++ src/pages/Home.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { FirstScreen } from '../components/FirstScreen';
+import { Featured } from '../components/Featured';
+
+import Jewerly1 from '../img/jewerly_1.png';
+import Jewerly2 from '../img/jewerly_2.png';
+import Jewerly3 from '../img/jewerly_3.png';
+import Jewerly4 from '../img/jewerly_4.png';
+
+export const Home = () => {
+    return (
+        <>
+            <FirstScreen />
+            <Featured cards={cards} />
+        </>
+    );
+};
+
+const cards = [
+    {
+        id: 0,
+        category: 'gold ring',
+        name: 'cassandra',
+        about: 'Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus',
+        price: '399',
+        img: Jewerly1,
+    },
+    {
+        id: 1,
+        category: 'ring',
+        name: 'diamond rings',
+        about: 'Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus',
+        price: '2,929',
+        img: Jewerly2,
+    },
+    {
+        id: 2,
+        category: 'necklace',
+        name: 'pearle necklace',
+        about: 'Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus',
+        price: '1,299',
+        img: Jewerly3,
+    },
+    {
+        id: 3,
+        category: 'bracelet',
+        name: 'stones bracelet',
+        about: 'Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus',
+        price: '1,329',
+        img: Jewerly4,
+    },
+];
