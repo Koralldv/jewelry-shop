59657732253ee74aea261012aa42f0ec15e5b560
Koralldv <52466177+Koralldv@users.noreply.github.com>
2022-01-03 15:25:54 +0300

    Удалил лишнее, организовал роутинг страниц

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import {Routes, Route} from 'react-router-dom'
+import {Layout} from './components/Layout'
+import {Home} from './pages/Home'
+import {About} from './pages/About'
+import {NotFound} from './pages/NotFound'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Home/>} />
+          <Route path='about' element={<About/>} />
+          <Route path='*' element={<NotFound/>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
