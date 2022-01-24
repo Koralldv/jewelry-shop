66e9450c559ffb7c6a7649a2659369c37a500364
Koralldv <52466177+Koralldv@users.noreply.github.com>
2022-01-24 13:07:40 +0300

    Страница Контактов

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Form } from '../components/Form';
+import { Map } from '../components/Map';
+
+export const Contacts = () => {
+    return (
+        <>
+            <Form />
+            <Map />
+        </>
+    );
+};
