ac499314688890b4a3d6a8fd49cbb635fc7c74a9
Koralldv <52466177+Koralldv@users.noreply.github.com>
2022-01-24 13:11:24 +0300

    Изменил разметку

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useMatch } from 'react-router-dom';
 import { Menu } from './Menu';
 import { NavBar } from './NavBar';
+import { BreadCrumbs } from './BreadCrumbs';
+import { Footer } from './Footer';
 import {
     AiFillYoutube,
     AiFillFacebook,
@@ -11,7 +13,100 @@ import {
     AiFillInstagram,
     AiOutlineDribbble,
 } from 'react-icons/ai';
-import { Footer } from './Footer';
+
+const breadCrumbsList = [
+    {
+        preTitle: 'who we are',
+        title: 'about jewerler',
+    },
+    {
+        preTitle: 'shop',
+        title: 'our jewelries',
+    },
+    {
+        preTitle: 'great deals',
+        title: 'our offers',
+    },
+
+    {
+        preTitle: 'need help?',
+        title: 'contact us',
+    },
+];
+
+export const Layout = () => {
+    const [breadCrumbs, setBreadCrumbs] = useState([]);
+    const [isHome, setIsHome] = useState(null);
+    const [isActiveMenu, setIsActiveMenu] = useState(false);
+    const [widthScreen, setWidthScreen] = useState(null);
+    const [showMenu, setShowMenu] = useState(null);
+
+    useEffect(() => {
+        window.onscroll = function () {
+            let location = window.pageYOffset;
+            if (location < 176) {
+                setShowMenu(false);
+            } else {
+                setShowMenu(true);
+            }
+        };
+    }, []);
+
+    useEffect(() => {
+        setWidthScreen(window.innerWidth);
+        function handleResize(e) {
+            setWidthScreen(e.currentTarget.innerWidth);
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const matches = {};
+    matches.main = useMatch('');
+    matches.about = useMatch('/about');
+    matches.jewelry = useMatch('/jewelry');
+    matches.offers = useMatch('/offers');
+    matches.contact = useMatch('/contacts');
+
+    useEffect(() => {
+        if (matches.main) {
+            setIsHome(true);
+        } else if (matches.about) {
+            setIsHome(false);
+            setBreadCrumbs(breadCrumbsList[0]);
+        } else if (matches.jewelry) {
+            setIsHome(false);
+            setBreadCrumbs(breadCrumbsList[1]);
+        } else if (matches.offers) {
+            setIsHome(false);
+            setBreadCrumbs(breadCrumbsList[2]);
+        } else if (matches.contact) {
+            setIsHome(false);
+            setBreadCrumbs(breadCrumbsList[3]);
+        }
+    }, [matches]);
+
+    return (
+        <>
+            <TopWrapper bg={showMenu}>
+                <Menu
+                    socialList={socialList}
+                    setIsActiveMenu={setIsActiveMenu}
+                    isActiveMenu={isActiveMenu}
+                    showMenu={showMenu}
+                    widthScreen={widthScreen}
+                />
+                {(isActiveMenu === true || (widthScreen > 767 && !showMenu)) && (
+                    <NavBar pages={pages} setIsActiveMenu={setIsActiveMenu} />
+                )}
+            </TopWrapper>
+            {!isHome && <BreadCrumbs breadCrumbs={breadCrumbs} />}
+            <Outlet />
+            <Footer socialList={socialList} />
+        </>
+    );
+};
 
 const socialList = [
     {
@@ -56,8 +151,8 @@ const pages = [
         path: '/about',
     },
     {
-        name: 'jewerly',
-        path: '/jewerly',
+        name: 'jewelry',
+        path: '/jewelry',
     },
     {
         name: 'offers',
@@ -73,58 +168,9 @@ const pages = [
     },
 ];
 
-export const Layout = () => {
-    const [isActiveMenu, setIsActiveMenu] = useState(false);
-
-    const [widthScreen, setWidthScreen] = useState(null);
-
-    const [showMenu, setShowMenu] = useState(null);
-
-    useEffect(() => {
-        window.onscroll = function () {
-            let location = window.pageYOffset;
-            if (location < 176) {
-                setShowMenu(false);
-            } else {
-                setShowMenu(true);
-            }
-        };
-    }, []);
-
-    useEffect(() => {
-        setWidthScreen(window.innerWidth);
-        function handleResize(e) {
-            setWidthScreen(e.currentTarget.innerWidth);
-        }
-
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    return (
-        <>
-            <TopWrapper bg={showMenu}>
-                <Menu
-                    socialList={socialList}
-                    setIsActiveMenu={setIsActiveMenu}
-                    isActiveMenu={isActiveMenu}
-                    showMenu={showMenu}
-                    widthScreen={widthScreen}
-                />
-                {(isActiveMenu === true || (widthScreen > 767 && !showMenu)) && (
-                    <NavBar pages={pages} setIsActiveMenu={setIsActiveMenu} />
-                )}
-            </TopWrapper>
-            <Outlet />
-            <Footer socialList={socialList} />
-        </>
-    );
-};
-
 const TopWrapper = styled.div`
     position: fixed;
     width: 100%;
     z-index: 999;
-
     background-color: ${(props) => props.bg && 'var(--black)'};
 `;
