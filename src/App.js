c2c6eeac407a056f01dddad5cb279b5a8a59f19c
Koralldv <52466177+Koralldv@users.noreply.github.com>
2022-02-03 20:05:00 +0300

    Подключил новые страницы

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import {Routes, Route} from 'react-router-dom'
 import {Layout} from './components/Layout'
 import {Home} from './pages/Home'
 import {About} from './pages/About'
+import {Offers} from './pages/Offers'
+import {Contacts} from './pages/Contacts'
+import {Jewelry} from './pages/Jewelry'
+import {JewelrySingle} from './pages/JewelrySingle'
 import {NotFound} from './pages/NotFound'
+import {ScrollToTop} from './components/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
+      <ScrollToTop>
       <Routes>
         <Route path='/' element={<Layout/>}>
           <Route index element={<Home/>} />
           <Route path='about' element={<About/>} />
+          <Route path='offers' element={<Offers/>} />
+          <Route path='jewelry' element={<Jewelry/>} />
+          <Route path='jewelry/:id' element={<JewelrySingle/>} />
           <Route path='*' element={<NotFound/>} />
+          <Route path='contacts' element={<Contacts/>} />
         </Route>
       </Routes>
+      </ScrollToTop>
     </div>
   );
 }
