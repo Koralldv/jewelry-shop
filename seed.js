8d4d14d34c4841c8ccbd77f67fb3705a555d6c01
Koralldv <52466177+Koralldv@users.noreply.github.com>
2022-02-03 19:57:06 +0300

    Обновил шаблон фейк бд

--- seed.js
+++ seed.js
@@ -3,15 +3,21 @@ const fs = require('fs')
 
 const { price , productName, productDescription } = faker.commerce
 
-const products = [...Array(30).keys()].map((_, i) => ({
+const productType = ['earrings', 'rings', 'bracelets', 'pendants', 'brooch']
+
+const productBrooch = ['gold', 'silver', 'pink gold']
+
+let products = [...Array(100).keys()].map((_, i) => ({
     id: i + 1,
     title: productName(),
     price: price(),
     description: productDescription(),
-    imageUrl: 'https://picsum.photos/200/200'
+    imageUrl: 'https://picsum.photos/200/200',
+    productType : productType[i % 5],
+    productBrooch : productBrooch[i % 3],
 }))
 
-const json = {products}
+const json = {products, totalCount: [products.length]}
 
 fs.writeFileSync('./db.json', JSON.stringify(json), 'utf-8', () => {
     console.log('success')
