e05c620f6fd5c646e5ae54ee01abe20763dd30d9
Koralldv <52466177+Koralldv@users.noreply.github.com>
2022-02-07 23:29:40 +0300

    Ссылка на заказы

--- src/api.js
+++ src/api.js
@@ -5,3 +5,5 @@ export const PRODUCTS = (limit, page) => BASE_URL + 'products?_limit=' + limit +
 export const COUNTS = BASE_URL + 'totalCount';
 
 export const SINGLE_PRODUCT = (id) => BASE_URL + 'products/' + id;
+
+export const ORDER = BASE_URL + 'order'
