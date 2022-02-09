b28ef195e718c3143d2c803c3a4600882fc1d598
Koralldv <52466177+Koralldv@users.noreply.github.com>
2022-02-09 16:41:00 +0300

    Удалил лишний код

--- src/components/Best.jsx
+++ src/components/Best.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import { Button } from './Button';
 import BestImg from '../img/best_gifts.png';
 import Diamond from '../img/diamond.png';
