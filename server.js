ff3e618939f6b27b9e4841de7b79c5d9c0d1d632
Koralldv <52466177+Koralldv@users.noreply.github.com>
2022-02-10 22:10:46 +0300

    именил путь

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json')
 const middlewares = jsonServer.defaults({
-    static: '/build',
+    static: './build',
 });
 
 const PORT = process.env.PORT || 3001;
