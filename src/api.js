f98b2df56af58dd4f6fc34163ae3065b5a4ae664
Koralldv <52466177+Koralldv@users.noreply.github.com>
2022-02-13 21:58:00 +0300

    измени путь к api

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001/'; 
+const BASE_URL = window.location.href + '/'; 
 
 export const PRODUCTS = (limit, page) => BASE_URL + 'products?_limit=' + limit + '&_page=' + page;
 
