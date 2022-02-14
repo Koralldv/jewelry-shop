10f34f0b62787160d4860efb879dccf1c29e8ee6
Koralldv <52466177+Koralldv@users.noreply.github.com>
2022-02-15 00:24:42 +0300

    Страница 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Button } from '../components/Button';
+import { useNavigate } from 'react-router-dom';
 
 export const NotFound = () => {
-    return <div>404</div>;
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <Title>page not found</Title>
+            <Button onClick={() => navigate(-1)} position="1rem auto 2rem">
+                Back
+            </Button>
+        </>
+    );
 };
+
+const Title = styled.h5`
+    font-family: var(--familyDomine);
+    font-weight: var(--fw-normal);
+    font-size: var(--fz-xl);
+    text-transform: capitalize;
+    color: var(--white);
+    padding: 2rem;
+    text-align: center;
+`;
