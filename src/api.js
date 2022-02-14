8db7f5eb7376ddcd7a882dad124daf3e1d53a02c
Koralldv <52466177+Koralldv@users.noreply.github.com>
2022-02-15 00:25:26 +0300

    Получение общей ссылки на api

--- src/api.js
+++ src/api.js
@@ -1,4 +1,12 @@
-const BASE_URL = window.location.protocol + '//' + window.location.hostname + '/'; 
+const getBaseUrl = () => {
+    if (window.location.hostname == 'localhost') {
+        return window.location.protocol + '//' + window.location.hostname + ':3001/';
+    }
+    return window.location.protocol + '//' + window.location.hostname + '/';
+}
+
+
+const BASE_URL = getBaseUrl();
 
 export const PRODUCTS = (limit, page) => BASE_URL + 'products?_limit=' + limit + '&_page=' + page;
 
